2fdc4218be0f2cc7589809e03ed73cc63d96ce0f
Het <govanihet09@gmail.com>
2025-03-27 11:33:42 +0530

    commit

---
+++ base/com/vo/user_register.py
@@ -0,0 +1,21 @@
+from base import db
+
+class UserRegister(db.Model):
+    __tablename__ = 'user_register'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    phone = db.Column(db.String(120), unique=True, nullable=False)
+
+
+    def as_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'phone': self.phone,
+            'password': self.password
+        }
+
+    db.create_all()
