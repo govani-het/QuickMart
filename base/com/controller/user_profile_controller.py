11f0d4798b1c9b69c157c76159b4809229ed998e
Het <govanihet09@gmail.com>
2025-04-06 17:02:19 +0530

    commit

--- base/com/controller/user_profile_controller.py
+++ base/com/controller/user_profile_controller.py
@@ -1,20 +1,47 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,session
+
+from base.com.dao.user_address_dao import UserAddressDAO
+from base.com.dao.user_order_dao import OrderDAO
+from base.com.dao.area_dao import AreaDAO
+from base.com.dao.city_dao import CityDAO
+
+
 from base import app
 
 @app.route('/user/profile')
 def user_profile():
-    return render_template('user/userDashboard.html')
+    user_dao = UserAddressDAO()
+
+    user_id = session.get('user_id')
+
+    user_data = user_dao.getUserinfo(user_id)
+    return render_template('user/userDashboard.html',user_data=user_data)
 
 
 @app.route('/user/userInfo')
 def user_parsonalInfo():
 
-    return render_template('user/userInfo.html')
+    user_dao = UserAddressDAO()
+    city_dao = CityDAO()
+    area_dao = AreaDAO()
+
+    user_id = session.get('user_id')
+
+
+    city = city_dao.view_city()
+    area = area_dao.view_area()
+    user_address_info = user_dao.view_address(user_id)
+    user_info = user_dao.getUserinfo(user_id)
+    return render_template('user/userInfo.html',user_info=user_info,user_address_info=user_address_info,city=city,area=area)
 
 @app.route('/user/order')
 def user_order():
+    order_dao = OrderDAO()
 
-    return render_template('user/order.html')
+    user_id = session.get('user_id')
+    order_data = order_dao.view_order(user_id)
+    print(order_data)
+    return render_template('user/order.html',order_data=order_data)
 
 @app.route('/user/address')
 def user_address():
---
+++ base/com/dao/user_order_dao.py
@@ -0,0 +1,12 @@
+from base import db
+
+from base.com.vo.user_order_vo import OrderVO
+from base.com.vo.user_order_item_vo import OrderItemVO
+from base.com.vo.product_vo import ProductVO
+
+class OrderDAO:
+
+    def view_order(self,user_id):
+
+        order_data = db.session.query(ProductVO, OrderVO,OrderItemVO).join(OrderItemVO,OrderVO.order_id==OrderItemVO.order_id).join(ProductVO,OrderItemVO.product_id==ProductVO.product_id).filter(OrderVO.user_id==user_id).all()
+        return order_data
